064927e44924445bb37bb33ff46b0644c8a416ba
valll94 <valeri.rangelov@sirma.bg>
2018-05-31 14:02:29 +0300

    adding progress bar

--- main.go
+++ main.go
@@ -5,33 +5,55 @@ import (
 	"io"
 	"log"
 	"os"
+	"time"
 )
 
+var FILESIZE int64
+
+var Sourcepath string
+var Destinationpath string
+
+var BYTESWRITTEN int64
+
 func main() {
+	BYTESWRITTEN = 0
+
 	if len(os.Args) != 3 {
 		log.Fatal("Please provice source and destination files")
 	}
-	sourcepath := os.Args[1]
-	destinationpath := os.Args[2]
-	if _, err := os.Stat(sourcepath); err != nil {
-		log.Fatal("Source file do not exist")
+	Sourcepath = os.Args[1]
+	Destinationpath := os.Args[2]
+	if sourceFileStat, err := os.Stat(Sourcepath); err != nil {
+		log.Fatal("Source file ", Sourcepath, " do not exist")
+	} else {
+		FILESIZE = sourceFileStat.Size()
 	}
-	if _, err := os.Stat(destinationpath); err == nil {
-		log.Fatal("Destination file exist")
+
+	if _, err := os.Stat(Destinationpath); err == nil {
+		log.Fatal("Destination file ", Destinationpath, " exist")
 	}
-	source, err := os.Open(sourcepath)
+	source, err := os.Open(Sourcepath)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer source.Close()
 
-	destination, _ := os.OpenFile(destinationpath, os.O_RDWR|os.O_CREATE, 0666)
+	destination, _ := os.OpenFile(Destinationpath, os.O_RDWR|os.O_CREATE, 0666)
 	defer destination.Close()
+	go func() {
+		for {
+			time.Sleep(time.Second)
+			BYTESWRITTEN, _ := os.Stat(Destinationpath)
+			progress := float64(BYTESWRITTEN.Size()) / float64(FILESIZE) * 100
+			fmt.Printf("\rProgress: %.2f%%", progress)
 
+		}
+	}()
 	_, err = io.Copy(destination, source)
 	if err != nil {
 		log.Fatal(err)
 	} else {
-		fmt.Println("Copying from", sourcepath, "to", destinationpath, "has been successfull!")
+		fmt.Println("\nCopying from", Sourcepath, "to", Destinationpath, "has been successfull!")
 	}
+
 }
