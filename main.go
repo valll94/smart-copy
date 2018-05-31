8818a2f527ab8f94588343dfa2e41cc93549c095
valll94 <valeri.rangelov@sirma.bg>
2018-05-31 10:28:33 +0300

    v0.1

--- main.go
+++ main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"log"
 	"os"
 )
 
 func main() {
-	source, _ := os.Open("./test")
+	if len(os.Args) != 3 {
+		log.Fatal("Please provice source and destination files")
+	}
+	sourcepath := os.Args[1]
+	destinationpath := os.Args[2]
+	source, err := os.Open(sourcepath)
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer source.Close()
 
-	destination, _ := os.OpenFile("./asd", os.O_RDWR|os.O_CREATE, 0666)
+	destination, _ := os.OpenFile(destinationpath, os.O_RDWR|os.O_CREATE, 0666)
 	defer destination.Close()
 
-	_, _ = io.Copy(destination, source)
+	_, err = io.Copy(destination, source)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("Copying from", sourcepath, "to", destinationpath, "has been successfull!")
+	}
 }
